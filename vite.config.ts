2c4c9cccd781eac25fd86ad960d0b10ee4e30894
OtabekMirzakhmedov <otabek.mirzakhmedov@gmail.com>
2026-03-25 11:39:51 +0500

    feat: starting react

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    plugins: [react()],
     base: '/online-zoo/',
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+            '@components': resolve(__dirname, 'src/components'),
+            '@pages': resolve(__dirname, 'src/pages'),
+            '@hooks': resolve(__dirname, 'src/hooks'),
+            '@contexts': resolve(__dirname, 'src/contexts'),
+            '@assets': resolve(__dirname, 'assets')
+        }
+    },
     build: {
         rollupOptions: {
             input: {
